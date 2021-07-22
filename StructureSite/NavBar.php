3eb6224a85826a7f6a1037c9429eedd59b7c12d6
Lingling Tabuteau <lingling.tabuteau@indricks.com>
2021-07-22 13:46:13 +0200

    add methode php include

---
+++ StructureSite/NavBar.php
@@ -0,0 +1,22 @@
+ <!-- navbar -->
+ <nav class="navbar navbar-expand-lg navbar-light "  style="background-color: SkyBlue;">
+  <div class="container-fluid">
+  <a class="navbar-brand" href="#">
+      <img src="/photo/smiling-pig.png" alt="logo" width="30" height="24">
+    </a>
+    <div class="navbar-nav">
+</div>
+    <form class="d-flex">
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+      <div class="navbar-nav">
+        <a class="nav-link active" aria-current="page" href="#">Acceuil</a>
+        <a class="nav-link" href="/BlogPosts.php">Blog Posts</a>
+        <a class="nav-link" href="/Admins/Signup.php">Se connecter</a>
+      </div>
+      </form>
+    </div>
+  </div>
+</nav>
